5ec3767e863dad353d0203c534e89684f05c026b
fanyongkang87 <fanyongkang87@qq.com>
2019-04-03 11:09:48 +0800

    remove the demo part

--- package.sh
+++ package.sh
@@ -7,4 +7,4 @@ rm -rf src/ykfan_utils.egg-info
 # python3 -m pip install --user --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
 # python3 -m pip install --user --upgrade twine
-# python3 -m twine upload -u ykfan87 dist/*
+python3 -m twine upload -u ykfan87 dist/*
